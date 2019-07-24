f91ef636c159096956345d72e54ff06d31dbfa4f
Hilde <mhilde@gmx.de>
2019-07-24 21:29:05 +0200

    refs #1

--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,6 @@
 ﻿micha = "Hallo Hilde, das läuft super!"
 
-print("Hello, World! Hello, Hilde!")
+print("Hello, World! Hello, Hilde!") #Diese Zeile gibt einen Text auf de Konsole aus. 
 
 print(micha)
 
